cd4e3fbc8d752f061ddf0e0537ac0490c9c159f2
JavaMarathon2021 <kristian2004@mail.ru>
2022-04-20 18:50:33 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,49 @@
 package day5;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
+        Car volkswagen = new Car();
+        volkswagen.setModel("Tuareg");
+        volkswagen.setColor("Black");
+        volkswagen.setYearOfProduction(1998);
 
+        volkswagen.getModel();
+        volkswagen.getColor();
+        volkswagen.getYearOfProduction();
+    }
+}
+
+class Car {
+    private String model;
+    private String color;
+    private int yearOfProduction;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        System.out.println(model);
+        return model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        System.out.println(color);
+        return color;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public int getYearOfProduction() {
+        System.out.println(yearOfProduction);
+        return yearOfProduction;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,36 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike("Suzuki", "Black",1985);
+        motorbike1.getModel();
+        motorbike1.getColor();
+        motorbike1.getYearOfProduction();
+    }
+}
 
+class Motorbike {
+    private String model;
+    private String color;
+    private int yearOfProduction;
+
+    public Motorbike (String model, String color, int yearOfProduction) {
+        this.model = model;
+        this.color = color;
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public String getModel() {
+        System.out.println(model);
+        return model;
+    }
+
+    public String getColor() {
+        System.out.println(color);
+        return color;
+    }
+
+    public int getYearOfProduction() {
+        System.out.println(yearOfProduction);
+        return yearOfProduction;
     }
 }
