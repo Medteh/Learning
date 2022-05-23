d868f7618fb245f56e941dc8a8244fe25548c5e0
JavaMarathon2021 <kristian2004@mail.ru>
2022-05-23 13:20:19 +0300

    correct day 16,task 1

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -6,29 +6,28 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.nio.file.FileSystemException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Task1 {
-    public static void printResult() throws FileNotFoundException {
+    public static void printResult() {
         File file = new File ("taskDay16");
         try {
             if (file.length() == 0) throw new FileSystemException("File is empty");
             Scanner scanner = new Scanner(file);
             String listNumber = scanner.nextLine();
             String[] arrayNumber = listNumber.split(" ");
-            List<Integer> listOfNumber = new ArrayList<>();
             double sumOfNumbers = 0;
             double averageNumbers = 0;
-            for (int i = 0; i < arrayNumber.length; i++) {
-                listOfNumber.add(Integer.parseInt(arrayNumber[i]));
-                sumOfNumbers += listOfNumber.get(i);
-            }
-            averageNumbers = sumOfNumbers / listOfNumber.size();
+            for (int i = 0; i < arrayNumber.length; i++)
+                sumOfNumbers += Integer.parseInt(arrayNumber[i]);
+
+            averageNumbers = sumOfNumbers / arrayNumber.length;
             BigDecimal averageNumbersThree = new BigDecimal(averageNumbers);
             averageNumbersThree = averageNumbersThree.setScale(3, BigDecimal.ROUND_CEILING);
 
-            System.out.println(listOfNumber);
+            System.out.println(Arrays.toString(arrayNumber));
             System.out.println(averageNumbers);
             System.out.println(averageNumbersThree);
 
