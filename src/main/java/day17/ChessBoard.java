dc9e365614c554e527ce54331db308b5e32ec3b0
JavaMarathon2021 <kristian2004@mail.ru>
2022-06-15 20:37:32 +0300

    day 17,dopolnil task2

--- src/main/java/day17/ChessBoard.java
+++ src/main/java/day17/ChessBoard.java
@@ -3,19 +3,14 @@ package day17;
 import java.util.Arrays;
 
 public class ChessBoard {
-    final int HORIZONTAL = 8;
-    final int VERTICAL = 8;
-    private ChessPiece[][] chessboard = new ChessPiece[HORIZONTAL][VERTICAL];
+    static final int HORIZONTAL = 8;
+    static final int VERTICAL = 8;
 
-    public ChessBoard(ChessPiece[][] chessboard) {
-        this.chessboard = chessboard;
-    }
-
-    void print() {
+    static void print(ChessPiece[][] chessBoard) {
         for (int i = 0; i< VERTICAL; i++) {
             System.out.println();
             for (int j = 0; j < HORIZONTAL; j++) {
-                System.out.print(chessboard[i][j].getFogureView());
+                System.out.print(chessBoard[i][j].getFogureView());
             }
         }
     }
--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -11,8 +11,41 @@ public class Task2 {
                 {ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.QUEEN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE},
                 {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.KING_WHITE, ChessPiece.EMPTY}
                 };
-        ChessBoard chessBoard = new ChessBoard(chessBoard1);
-        chessBoard.print();
+
+        ChessPiece[][] chessBoard2 = new ChessPiece[ChessBoard.HORIZONTAL][ChessBoard.VERTICAL];
+        chessBoard2[0][0] = ChessPiece.ROOK_BLACK;
+        chessBoard2[0][5] = ChessPiece.ROOK_BLACK;
+        chessBoard2[0][6] = ChessPiece.KING_BLACK;
+        chessBoard2[1][1] = ChessPiece.ROOK_WHITE;
+        chessBoard2[1][4] = ChessPiece.PAWN_BLACK;
+        chessBoard2[1][5] = ChessPiece.PAWN_BLACK;
+        chessBoard2[1][7] = ChessPiece.PAWN_BLACK;
+        chessBoard2[2][0] = ChessPiece.PAWN_BLACK;
+        chessBoard2[2][2] = ChessPiece.KING_BLACK;
+        chessBoard2[2][6] = ChessPiece.PAWN_BLACK;
+        chessBoard2[3][0] = ChessPiece.QUEEN_BLACK;
+        chessBoard2[3][3] = ChessPiece.BISHOP_WHITE;
+        chessBoard2[4][3] = ChessPiece.BISHOP_BLACK;
+        chessBoard2[4][4] = ChessPiece.PAWN_WHITE;
+        chessBoard2[5][4] = ChessPiece.BISHOP_WHITE;
+        chessBoard2[5][5] = ChessPiece.PAWN_WHITE;
+        chessBoard2[6][0] = ChessPiece.PAWN_WHITE;
+        chessBoard2[6][3] = ChessPiece.QUEEN_WHITE;
+        chessBoard2[6][5] = ChessPiece.PAWN_WHITE;
+        chessBoard2[6][7] = ChessPiece.PAWN_WHITE;
+        chessBoard2[7][5] = ChessPiece.ROOK_WHITE;
+        chessBoard2[7][6] = ChessPiece.KING_WHITE;
+
+        for (int i=0; i < ChessBoard.HORIZONTAL; i++) {
+            for (int j = 0; j < ChessBoard.VERTICAL; j++) {
+                if (chessBoard2[i][j] == null) chessBoard2[i][j] = ChessPiece.EMPTY;
+            }
+        }
+
+        ChessBoard.print(chessBoard1);
+        System.out.println();
+        System.out.println();
+        ChessBoard.print(chessBoard2);
     }
 }
 
