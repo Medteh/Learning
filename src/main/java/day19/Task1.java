5d038d10c4d52a76a7bd2f515802f2d7cf25c502
JavaMarathon2021 <kristian2004@mail.ru>
2022-06-25 00:29:28 +0300

    day 19,task1, task2

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,45 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        String path = "D:\\JavaMarathon2021\\JavaMarathon2021\\src\\main\\resources\\dushi.txt";
+        Scanner scanner = new Scanner(new File (path), "UTF-8");
+        scanner.useDelimiter("[.,:;()?!\"\\s–]+");
 
+        List<String> text= new ArrayList<>();
+            while (scanner.hasNext()) text.add(scanner.next());
+        Map<String, Integer> textMap = new HashMap<>();
+            for (String list: text) {
+                if (textMap.keySet().contains(list)) {
+                    textMap.put(list, textMap.get(list) + 1);
+                }else textMap.put(list, 1);
+            }
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(textMap.entrySet());
+            list.sort(new Comparator<Map.Entry<String, Integer>>() {
+                @Override
+                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                    if (o1.getValue() > o2.getValue()) {
+                        return -1;
+                    } else if (o1.getValue() < o2.getValue()) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+        int j = 1;
+        for (Object entries: list) {
+            if (j == 101) break;
+            System.out.println(j+ "    " + entries);
+            j++;
+        }
+        System.out.println("Слово Чичиков в тексте встречается " + textMap.get("Чичиков") + " раз");
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,11 +1,51 @@
 package day19.task2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String path = "D:\\JavaMarathon2021\\JavaMarathon2021\\src\\main\\resources\\taxi_cars.txt";
+        Scanner scanner = new Scanner(new File(path), "UTF-8");
+        Map<Integer, Point> taxiCars = new TreeMap<>();
 
+        while (scanner.hasNextLine()) {
+            String[] strArray = scanner.nextLine().split(" ");
+            int[] intArr = new int[strArray.length];
+                for (int i = 0; i < strArray.length; i++)
+                    intArr[i] = Integer.parseInt(strArray[i]);
+            taxiCars.put(intArr[0], new Point(intArr[1], intArr[2]));
+        }
+
+        Scanner scannerX1 = new Scanner(System.in);
+            System.out.println("Введите координаты первой точки");
+                int x1 = scannerX1.nextInt();
+                int y1 = scannerX1.nextInt();
+                System.out.println("x1 = " + x1 + ", y1 = " + y1);
+            System.out.println("Введите координаты второй точки");
+                int x2 = scannerX1.nextInt();
+                int y2 = scannerX1.nextInt();
+                System.out.println("x2 = " + x2 + ", y2 = " + y2);
+
+        List <Integer> listOfCars = new ArrayList<>();
+            int j = 0;
+            for (Map.Entry entries: taxiCars.entrySet()) {
+                Point point = (Point) entries.getValue();
+                if (point.getX() > x1 && point.getY() > y1 &&
+                        point.getX() < x2 && point.getY() < y2) {
+                    listOfCars.add((Integer) entries.getKey());
+                    j++;
+                }
+            }
+            System.out.println(listOfCars);
+            System.out.println("Всего в заданном квадрате находятся " + j + " машин");
     }
 }
 
